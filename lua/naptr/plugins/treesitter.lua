0cc09abdc5a06f5f14dc9eb26da6149359844c05
Krishna Eka Yusuf Putra <krishna.putra332@gmail.com>
2023-12-21 15:47:52 +0700

    Add treesitter context plugin

--- lua/naptr/plugins/treesitter.lua
+++ lua/naptr/plugins/treesitter.lua
@@ -1,25 +1,31 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-        "JoosepAlviste/nvim-ts-context-commentstring",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            "JoosepAlviste/nvim-ts-context-commentstring",
+        },
+        build = ":TSUpdate",
+        config = function ()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                ensure_installed = {
+                    "javascript", "typescript", "vue", "html", "tsx", "lua","rust",
+                    "zig", "css", "scss", "vimdoc", "cpp", "yuck", "bash", "c_sharp",
+                    "yaml", "dockerfile", "go"
+                },
+                sync_install = false,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false
+                },
+                indent = {
+                    enable = true
+                }
+            })
+        end
     },
-    build = ":TSUpdate",
-    config = function ()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            ensure_installed = {
-                "javascript", "typescript", "vue", "html", "tsx", "lua","rust",
-                "zig", "css", "scss", "vimdoc", "cpp", "yuck", "bash", "c_sharp",
-                "yaml", "dockerfile", "go"
-            },
-            sync_install = false,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false
-            },
-            indent = {
-                enable = true
-            }
-        })
-    end
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        build = ":TSContextEnable"
+    }
 }
