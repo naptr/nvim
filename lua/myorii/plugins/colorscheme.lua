a90c12fa8f36dac2ddc64369ea0c30021df57a8e
Krishna Eka Yusuf Putra <krishna.putra332@gmail.com>
2024-09-13 16:57:47 +0700

    Add mellow (colorscheme)

--- lua/myorii/plugins/colorscheme.lua
+++ lua/myorii/plugins/colorscheme.lua
@@ -8,9 +8,22 @@ return {
 		background = {
 		    dark = "dragon"
 		},
-		transparent = true
+		transparent = true,
 	    })
 	    vim.cmd([[ colorscheme kanagawa ]])
+	end
+    },
+    {
+	"mellow-theme/mellow.nvim",
+	config = function ()
+	    -- background
+	    vim.g.mellow_transparent = true
+
+	    -- texts
+	    vim.g.mellow_italic_keywords = true
+	    vim.g.mellow_italic_functions = true
+
+	    -- vim.cmd([[ colorscheme mellow ]])
 	end
     }
 }
