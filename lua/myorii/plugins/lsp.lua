6b9201e684882b5b508bd19e8a623588eb1e48d5
Krishna Eka Yusuf Putra <krishna.putra332@gmail.com>
2024-09-13 11:42:20 +0700

    Separate completion configuration with lsp configuration

--- lua/myorii/plugins/lsp.lua
+++ lua/myorii/plugins/lsp.lua
@@ -4,17 +4,9 @@ return {
 	dependencies = {
 	    "williamboman/mason.nvim",
 	    "williamboman/mason-lspconfig.nvim",
-	    "hrsh7th/cmp-nvim-lsp",
-	    "hrsh7th/cmp-buffer",
-	    "hrsh7th/cmp-path",
-	    "hrsh7th/cmp-cmdline",
-	    "hrsh7th/nvim-cmp",
-	    "hrsh7th/cmp-nvim-lsp-signature-help",
-	    "L3MON4D3/LuaSnip",
-	    "saadparwaiz1/cmp_luasnip",
+	    "hrsh7th/cmp-nvim-lsp"
 	},
 	config = function ()
-	    local cmp = require("cmp")
 	    local cmp_lsp = require("cmp_nvim_lsp")
 	    local capabilities = vim.tbl_deep_extend(
 		"force",
@@ -126,69 +118,6 @@ return {
 			})
 		    end
 		}
-	    })
-
-	    local cmp_select = { behavior = cmp.SelectBehavior.Select }
-	    cmp.setup({
-		experimental = { ghost_text = true },
-		snippet = {
-		    expand = function (args)
-			local luasnip = require("luasnip")
-			luasnip.lsp_expand(args.body) -- For `luasnip` user
-		    end
-		},
-		mapping = cmp.mapping.preset.insert({
-		    ["<Up>"] = cmp.mapping.select_prev_item(cmp_select),
-		    ["<Down>"] = cmp.mapping.select_next_item(cmp_select),
-		    ["<Tab>"] = cmp.mapping(function (fallback)
-			-- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
-			if cmp.visible() then
-			    local entry = cmp.get_selected_entry()
-			    if not entry then
-				cmp.select_next_item(cmp_select)
-			    end
-			    cmp.confirm()
-			else
-			    fallback()
-			end
-		    end, { "i", "s", "c" }),
-		    ["<C-Space>"] = cmp.mapping.complete()
-		}),
-		sources = cmp.config.sources({
-		    { name = "nvim_lsp" },
-		    { name = "luasnip" },
-		    { name = "nvim_lsp_signature_help" }
-		}, {
-		    { name = "buffer" },
-		    { name = "path" }
-		}),
-		view = {
-		    entries = {
-			name = "custom",
-			selection_order = "near_cursor"
-		    }
-		}
-	    })
-
-	    cmp.setup.cmdline({ "/", "?" }, {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = {
-		    { name = "buffer" }
-		}
-	    })
-
-	    cmp.setup.cmdline({ ":" }, {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = cmp.config.sources({
-		    { name = "path" }
-		}, {
-		    {
-			name = "cmdline",
-			option = {
-			    ignore_cmds = { "Man", "!" }
-			}
-		    }
-		})
 	    })
 	end
     }
