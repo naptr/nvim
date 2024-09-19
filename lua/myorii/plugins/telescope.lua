8656f5d02018f949058b8ae3f872743a0d4cb035
Krishna Eka Yusuf Putra <krishna.putra332@gmail.com>
2024-09-19 10:29:33 +0700

    Add telescope.nvim

---
+++ lua/myorii/plugins/telescope.lua
@@ -0,0 +1,19 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function ()
+	require("telescope").setup({
+	    defaults = {
+		file_ignore_patterns = {
+		    "node_modules"
+		}
+	    }
+	})
+
+	local builtin = require("telescope.builtin")
+
+	vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+	vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+	vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    end
+}
