ac9d8b7e516f72b639e29dd46badd31c933a0062
Krishna Eka Yusuf Putra <krishna.putra332@gmail.com>
2024-09-23 10:32:46 +0700

    Add search string in telescope

--- lua/myorii/plugins/telescope.lua
+++ lua/myorii/plugins/telescope.lua
@@ -15,5 +15,8 @@ return {
 	vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 	vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 	vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+	vim.keymap.set("n", "<leader>fs", function ()
+		builtin.grep_string({ search = vim.fn.input("Grep > ")})
+	end)
     end
 }
